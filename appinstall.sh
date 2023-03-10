567bb0cd7d634cc635f1e1f09b181d43b73be2b6
Roman Zelenin <rzelenin@gmail.com>
2023-03-10 04:46:40 +0000

    installation script

---
+++ appinstall.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+echo 'start  .........'
+
+echo 'npm install'
+npm install
+
+echo 'composer install'
+composer install
+
+echo 'app key:generate'
+php artisan key:generate
+
+echo 'npm run build'
+npm run build
+
+echo 'php artisan serve'
+php artisan serve
+
+echo '....... finish'
