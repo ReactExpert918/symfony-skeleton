2a1435deabacbe423915b502e387de903c5662cb
Roman Zelenin <rzelenin@gmail.com>
2023-03-10 13:37:31 +0000

    refactoring

--- app/Helpers/ApiClientHelper.php
+++ app/Helpers/ApiClientHelper.php
@@ -2,8 +2,11 @@
 
 namespace App\Helpers;
 
+use DateTime;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Class HtmlHelper.
@@ -32,6 +35,128 @@ class ApiClientHelper
         ]);
 
         return $client;
+    }
+
+    public static function authUser($request)
+    {
+        $data = $request->all();
+
+        $client = self::get(false);
+        try {
+            $response = $client->request('POST', '/api/v2/token', ['body' => json_encode($data)]);
+            $data_response = json_decode($response->getBody()->getContents(), true);
+            Cache::put('token_key', $data_response['token_key'], now()->addMinutes(1440));
+        } catch (RequestException $e) {
+            Log::error($e->getMessage());
+        }
+
+        // < request user data
+        $client = self::get();
+        try {
+            $response = $client->request('GET', '/api/v2/me');
+            $data_user = json_decode($response->getBody()->getContents(), true);
+            Cache::put('user_data', (object)$data_user, now()->addMinutes(1440));
+        } catch (RequestException $e) {
+            Log::error($e->getMessage());
+        }
+        // > request user data
+    }
+
+    public function getAuthorInfo($author_id) {
+        $client = self::get();
+        try {
+            $response = $client->request('GET', '/api/v2/authors/'.$author_id);
+            $response_data = json_decode($response->getBody()->getContents(), true);
+        } catch (RequestException $e) {
+            Log::error($e->getMessage());
+        }
+
+        return $response_data;
+    }
+
+
+    public function getAuthors() {
+        $client = self::get();
+        try {
+            $response = $client->request('GET', '/api/v2/authors', [
+                'query' => [
+                    'orderBy' => 'id',
+                    'direction' => 'ASC',
+                    'limit' => 12,
+                    'page' => 1
+                ]
+            ]);
+            $response_data = json_decode($response->getBody()->getContents(), true);
+        } catch (RequestException $e) {
+            Log::error($e->getMessage());
+        }
+
+        return $response_data;
+    }
+
+    public function authorStore($request) {
+        $data = $request->all();
+        $birthday = DateTime::createFromFormat('d/m/Y', $data['birthday']);
+        $data['birthday'] = $birthday->format('Y-m-d');
+
+        $client = self::get();
+        try {
+            $response = $client->request(
+                'POST',
+                '/api/v2/authors',
+                ['body' => json_encode($data)]
+            );
+        } catch (RequestException $e) {
+            Log::error($e->getMessage());
+        }
+
+        return $response;
+    }
+
+    public function authorDestroy($author) {
+        $client = self::get();
+        try {
+            $response = $client->delete('/api/v2/authors/'.$author);
+        } catch (RequestException $e) {
+            Log::error($e->getMessage());
+        }
+
+        return $response;
+    }
+
+    public function bookStore($request) {
+        $data = $request->all();
+
+        $data['author']['id'] = (int)$data['author']['id'];
+        $release_date = DateTime::createFromFormat('d/m/Y', $data['release_date']);
+        $data['release_date'] = $release_date->format('Y-m-d');
+        $data['number_of_pages'] = (int)$data['number_of_pages'];
+
+        $client = self::get();
+        try {
+            $response = $client->request(
+                'POST',
+                '/api/v2/books',
+                ['body' => json_encode($data)]
+            );
+        } catch (RequestException $e) {
+            Log::error($e->getMessage());
+        }
+
+        return $data;
+    }
+
+    public function bookDestroy($request, $book) {
+        $data = $request->all();
+
+        $client = self::get();
+        try {
+            $response = $client->delete('/api/v2/books/'.$book);
+        } catch (RequestException $e) {
+            Log::error($e->getMessage());
+        }
+
+        return $data;
     }
 
 }
--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,58 +5,21 @@ namespace App\Http\Controllers;
 use App\Helpers\ApiClientHelper;
 use App\Http\Requests\StoreAuthorRequest;
 use App\Http\Requests\StoreBookRequest;
-use DateTime;
-use GuzzleHttp\Exception\RequestException;
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
-use Illuminate\Support\Facades\Cache;
 
 class ApiController extends Controller
 {
 
     public function getToken(Request $request)
     {
-        $data = $request->all();
-
-        $client = ApiClientHelper::get(false);
-        try {
-            $response = $client->request('POST', '/api/v2/token', ['body' => json_encode($data)]);
-            $data_response = json_decode($response->getBody()->getContents(), true);
-            Cache::put('token_key', $data_response['token_key'], now()->addMinutes(1440));
-        } catch (RequestException $e) {
-            echo $e->getMessage();
-        }
-
-
-        // < request user data
-        $client = ApiClientHelper::get();
-        try {
-            $response = $client->request('GET', '/api/v2/me');
-            $data_user = json_decode($response->getBody()->getContents(), true);
-            Cache::put('user_data', (object)$data_user, now()->addMinutes(1440));
-        } catch (RequestException $e) {
-            echo $e->getMessage();
-        }
-        // > request user data
+        ApiClientHelper::authUser($request);
 
         return view('dashboard');
     }
 
     public function getAuthors() {
-        $client = ApiClientHelper::get();
-        try {
-            $response = $client->request('GET', '/api/v2/authors', [
-                'query' => [
-                    'orderBy' => 'id',
-                    'direction' => 'ASC',
-                    'limit' => 12,
-                    'page' => 1
-                ]
-            ]);
-            $response_data = json_decode($response->getBody()->getContents(), true);
-        } catch (RequestException $e) {
-            echo $e->getMessage();
-        }
+        $client = new ApiClientHelper;
+        $response_data = $client->getAuthors();
 
         return view('author.index')->withAuthors($response_data['items']);
     }
@@ -67,112 +30,53 @@ class ApiController extends Controller
     }
 
     public function authorStore(StoreAuthorRequest $request) {
-        $data = $request->all();
-        $birthday = DateTime::createFromFormat('d/m/Y', $data['birthday']);
-        $data['birthday'] = $birthday->format('Y-m-d');
-
-        $client = ApiClientHelper::get();
-        try {
-            $response = $client->request(
-                'POST',
-                '/api/v2/authors',
-                ['body' => json_encode($data)]
-            );
-        } catch (RequestException $e) {
-            echo $e->getMessage();
-        }
+        $client = new ApiClientHelper;
+        $response = $client->authorStore($request);
 
         return redirect()->route('authors')->withSuccess(__('Author was successfully created.'));
     }
 
     public function authorDestroy($author) {
-        $authorInfo = $this->getAuthorInfo($author);
+        $client = new ApiClientHelper;
+        $authorInfo = $client->getAuthorInfo($author);
 
         if (!empty($authorInfo['books'])) {
             return redirect()->route('authors')->withFail(__('You can\'t delete author that has books.'));
         }
         else {
-            $client = ApiClientHelper::get();
-            try {
-                $response = $client->delete('/api/v2/authors/'.$author);
-            } catch (RequestException $e) {
-                echo $e->getMessage();
-            }
+            $response = $client->authorDestroy($author);
 
             return redirect()->route('authors')->withSuccess(__('Author was successfully deleted.'));
         }
     }
 
     public function authorShow($author) {
-        $authorInfo = $this->getAuthorInfo($author);
+        $client = new ApiClientHelper;
+        $authorInfo = $client->getAuthorInfo($author);
 
         return view('author.show')->withAuthor($authorInfo);
     }
 
     public function bookCreate() {
-        $client = ApiClientHelper::get();
-        try {
-            $response = $client->request('GET', '/api/v2/authors', [
-                'query' => [
-                    'orderBy' => 'id',
-                    'direction' => 'ASC',
-                    'limit' => 12,
-                    'page' => 1
-                ]
-            ]);
-            $response_data = json_decode($response->getBody()->getContents(), true);
-        } catch (RequestException $e) {
-            echo $e->getMessage();
-        }
+        $client = new ApiClientHelper;
+        $response_data = $client->getAuthors();
 
         return view('book.create')->withAuthors($response_data['items']);
     }
 
     public function bookStore(StoreBookRequest $request) {
-        $data = $request->all();
-
-        $data['author']['id'] = (int)$data['author']['id'];
-        $release_date = DateTime::createFromFormat('d/m/Y', $data['release_date']);
-        $data['release_date'] = $release_date->format('Y-m-d');
-        $data['number_of_pages'] = (int)$data['number_of_pages'];
-
-        $client = ApiClientHelper::get();
-        try {
-            $response = $client->request(
-                'POST',
-                '/api/v2/books',
-                ['body' => json_encode($data)]
-            );
-        } catch (RequestException $e) {
-            echo $e->getMessage();
-        }
+        $client = new ApiClientHelper;
+        $data = $client->bookStore($request);
 
         return redirect()->route('authors.show', $data['author']['id'] )->withSuccess(__('Book was successfully created.'));
     }
 
     public function bookDestroy(Request $request, $book) {
-        $data = $request->all();
-
-        $client = ApiClientHelper::get();
-        try {
-            $response = $client->delete('/api/v2/books/'.$book);
-        } catch (RequestException $e) {
-            echo $e->getMessage();
-        }
+        $client = new ApiClientHelper;
+        $data = $client->bookDestroy($request, $book);
 
         return redirect()->route('authors.show', $data['author'])->withSuccess(__('Book was successfully deleted.'));
     }
 
-    public function getAuthorInfo($author_id) {
-        $client = ApiClientHelper::get();
-        try {
-            $response = $client->request('GET', '/api/v2/authors/'.$author_id);
-            $response_data = json_decode($response->getBody()->getContents(), true);
-        } catch (RequestException $e) {
-            echo $e->getMessage();
-        }
-
-        return $response_data;
-    }
 
 }
